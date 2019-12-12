3583be1e3a45bd3884c6f004360d4ad2b596adb7
bmcd87 <bradmcdermott@gmail.com>
2019-12-12 17:00:02 -0500

    Very rough working version that generates raft/istanbul example with a simple start script

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
+const exec = require('child_process').exec;
 const {
   INITIAL_MODE,
   NUMBER_NODES,
@@ -30,11 +31,12 @@ async function quickstart () {
   const config = {
     network: {
       name: `${numberNodes}-nodes-${consensus}-${deployment}`,
+      verbosity: 5,
       consensus: consensus,
       id: 10,
       permissioned: 'true',
-      genesis_file: `7nodes/${consensus}-genesis.json`,
-      generate_keys: 'false',
+      genesisFile: `7nodes/${consensus}-genesis.json`,
+      generateKeys: 'false',
     },
     nodes: []
   }
@@ -65,10 +67,80 @@ async function quickstart () {
 }
 
 function createNetwork (config) {
-  console.log(config)
+  // TODO let's make all these file system operations safe, so someone doesn't
+  // TODO accidentally/intentionally rm -rf network/../../../ :)
   let path = `network/${config.network.name}`
-  fs.mkdirSync(path, {recursive: true})
+  fs.rmdirSync(path, {recursive: true})
+  let qdata = `${path}/qdata`
+  fs.mkdirSync(qdata, {recursive: true})
+  fs.mkdirSync(`${qdata}/logs`)
   fs.writeFileSync(`${path}/config.json`, JSON.stringify(config, null, 2))
+
+  const staticNodes = generateStaticNodes(config.nodes, config.network.consensus)
+  const startCommands = []
+
+  config.nodes.forEach((node, i) => {
+    const nodeNumber = i + 1
+    const generatedKeyFolder = `7nodes/key${nodeNumber}`
+    let quorumDir = `${qdata}/dd${nodeNumber}`
+    let gethDir = `${quorumDir}/geth`
+    let keyDir = `${quorumDir}/keystore`
+    let tmDir = `${qdata}/c${nodeNumber}`
+    fs.mkdirSync(quorumDir)
+    fs.mkdirSync(gethDir)
+    fs.mkdirSync(keyDir)
+    fs.mkdirSync(tmDir)
+
+    fs.writeFileSync(`${quorumDir}/permissioned-nodes.json`, JSON.stringify(staticNodes, null, 2))
+    fs.writeFileSync(`${quorumDir}/static-nodes.json`, JSON.stringify(staticNodes, null, 2))
+    fs.copyFileSync(`${config.network.genesisFile}`, `${quorumDir}/genesis.json`)
+    fs.copyFileSync(`${generatedKeyFolder}/key`, `${keyDir}/key`)
+    fs.copyFileSync(`${generatedKeyFolder}/nodekey`, `${gethDir}/nodekey`)
+    fs.copyFileSync(`${generatedKeyFolder}/password.txt`, `${keyDir}/password.txt`)
+    fs.copyFileSync(`${generatedKeyFolder}/tm.key`, `${tmDir}/tm.key`)
+    fs.copyFileSync(`${generatedKeyFolder}/tm.pub`, `${tmDir}/tm.pub`)
+
+    const { verbosity, id, consensus } = config.network
+    const { devP2pPort, rpcPort, wsPort, raftPort } = node.quorum
+
+    const args =`--nodiscover --verbosity ${verbosity} --networkid ${id} --rpc --rpccorsdomain=* --rpcvhosts=* --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,${consensus},quorumPermission --emitcheckpoints --unlock 0 --password qdata/dd${nodeNumber}/keystore/password.txt`
+    console.log('consensus:', consensus)
+    const consensusArgs = consensus === 'raft' ?
+      `--raft --raftport ${raftPort}` :
+      `--istanbul.blockperiod 5 --syncmode full --mine --minerthreads 1`
+
+    const startCommand = `PRIVATE_CONFIG=ignore nohup geth --datadir qdata/dd${nodeNumber} ${args} ${consensusArgs} --permissioned --rpcport ${rpcPort} --port ${devP2pPort} 2>>qdata/logs/${nodeNumber}.log &`
+    startCommands.push(startCommand)
+  })
+
+  config.nodes.forEach((node, i) => {
+    // TODO figure out the safest way to run shell commands
+    exec(`cd ${path} && geth --datadir qdata/dd${i+1} init qdata/dd${i+1}/genesis.json`, (e, stdout, stderr)=> {
+      if (e instanceof Error) {
+        console.error(e);
+        throw e;
+      }
+    });
+  })
+  fs.writeFileSync(`${path}/start.sh`, startCommands.join("\n"))
+  fs.chmodSync(`${path}/start.sh`, "755");
+  fs.copyFileSync(`lib/stop.sh`, `${path}/stop.sh`)
+
+  exitMessage = `Network successfully created.\n\ncd ${path}\n./start.sh`
+}
+
+function generateStaticNodes (nodes, consensus) {
+  return nodes.map((node, i) => {
+    const nodeNumber = i + 1
+    const generatedKeyFolder = `7nodes/key${nodeNumber}`
+    const enodeId = fs.readFileSync(`${generatedKeyFolder}/enode`, 'utf8').trim()
+
+    let enodeAddress = `enode://${enodeId}@127.0.0.1:${node.quorum.devP2pPort}?discport=0`
+    if (consensus === 'raft') {
+      enodeAddress += `&raftport=${node.quorum.raftPort}`
+    }
+    return enodeAddress
+  })
 }
 
 
