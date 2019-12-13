a258a39046bae89b2973f3a35820f4ab6b69517b
bmcd87 <bradmcdermott@gmail.com>
2019-12-13 15:54:27 -0500

    Wraps filesystem operations so we can test, uses path module

---
+++ src/utils/fileUtils.js
@@ -0,0 +1,31 @@
+import fs from 'fs'
+import path from 'path'
+
+export function writeJsonFile (folder, filename, object, space = 2) {
+  fs.writeFileSync(path.join(folder, filename),
+    JSON.stringify(object, null, space))
+}
+
+export function writeFile (filePath, contents, executable = false) {
+  fs.writeFileSync(filePath, contents)
+  if (executable) {
+    fs.chmodSync(filePath, '755')
+  }
+}
+
+export function removeFolder (networkPath = '') {
+  if (networkPath === '' ||
+    networkPath === '/' ||
+    networkPath.indexOf(process.cwd()) !== 0) {
+    throw new Error('Tried to remove folder outside of working directory')
+  }
+  fs.rmdirSync(networkPath, { recursive: true })
+}
+
+export function createFolder (path, recursive = false) {
+  fs.mkdirSync(path, { recursive })
+}
+
+export function copyFile (src, dest) {
+  fs.copyFileSync(src, dest)
+}
--- src/utils/networkCreator.js
+++ src/utils/networkCreator.js
@@ -1,80 +1,88 @@
 import fs from 'fs'
+import path from 'path'
 import { exec } from 'child_process'
+import sanitize from 'sanitize-filename'
+import {
+  copyFile,
+  createFolder,
+  removeFolder,
+  writeFile,
+  writeJsonFile
+} from './fileUtils'
 
 export function createNetwork (config) {
-  // TODO let's make all these file system operations safe, so someone doesn't
-  // TODO accidentally/intentionally rm -rf network/../../../ :)
-  // TODO probably should use the built-in Path module + some validation
-  // TODO https://nodejs.org/en/knowledge/file-system/security/introduction/
-  let path = `network/${config.network.name}`
-  fs.rmdirSync(path, { recursive: true })
-  let qdata = `${path}/qdata`
-  fs.mkdirSync(`${qdata}/logs`, { recursive: true })
-  fs.writeFileSync(`${path}/config.json`, JSON.stringify(config, null, 2))
+  // https://nodejs.org/en/knowledge/file-system/security/introduction/
+  const networkFolderName = sanitize(config.network.name)
+  if (networkFolderName === '') {
+    throw new Error('Network name was empty or contained invalid characters')
+  }
 
-  const staticNodes = generateStaticNodes(config.nodes,
-    config.network.consensus)
+  const networkPath = path.join(process.cwd(), 'network', networkFolderName)
+  removeFolder(networkPath)
+
+  const qdata = path.join(networkPath, 'qdata')
+  const logs = path.join(qdata, 'logs')
+  createFolder(logs, true)
+  writeJsonFile(networkPath, 'config.json', config)
+
+  const staticNodes = createStaticNodes(config.nodes, config.network.consensus)
+  const initCommands = []
   const startCommands = []
+  const examplesPath = path.join(process.cwd(), '7nodes')
 
   config.nodes.forEach((node, i) => {
     const nodeNumber = i + 1
-    const generatedKeyFolder = `7nodes/key${nodeNumber}`
-    let quorumDir = `${qdata}/dd${nodeNumber}`
-    let gethDir = `${quorumDir}/geth`
-    let keyDir = `${quorumDir}/keystore`
-    let tmDir = `${qdata}/c${nodeNumber}`
-    fs.mkdirSync(quorumDir)
-    fs.mkdirSync(gethDir)
-    fs.mkdirSync(keyDir)
-    fs.mkdirSync(tmDir)
+    const keyFolder = path.join(examplesPath, `key${nodeNumber}`)
+    const quorumDir = path.join(qdata, `dd${nodeNumber}`)
+    const gethDir = path.join(quorumDir, `geth`)
+    const keyDir = path.join(quorumDir, `keystore`)
+    const tmDir = path.join(qdata, `c${nodeNumber}`)
+    const passwordDestination = path.join(keyDir, 'password.txt')
+    let genesisDestination = path.join(quorumDir, 'genesis.json')
+    createFolder(quorumDir)
+    createFolder(gethDir)
+    createFolder(keyDir)
+    createFolder(tmDir)
 
-    fs.writeFileSync(`${quorumDir}/permissioned-nodes.json`,
-      JSON.stringify(staticNodes, null, 2))
-    fs.writeFileSync(`${quorumDir}/static-nodes.json`,
-      JSON.stringify(staticNodes, null, 2))
-    fs.copyFileSync(`${config.network.genesisFile}`,
-      `${quorumDir}/genesis.json`)
-    fs.copyFileSync(`${generatedKeyFolder}/key`, `${keyDir}/key`)
-    fs.copyFileSync(`${generatedKeyFolder}/nodekey`, `${gethDir}/nodekey`)
-    fs.copyFileSync(`${generatedKeyFolder}/password.txt`,
-      `${keyDir}/password.txt`)
-    fs.copyFileSync(`${generatedKeyFolder}/tm.key`, `${tmDir}/tm.key`)
-    fs.copyFileSync(`${generatedKeyFolder}/tm.pub`, `${tmDir}/tm.pub`)
+    writeJsonFile(quorumDir, 'permissioned-nodes.json', staticNodes)
+    writeJsonFile(quorumDir, 'static-nodes.json', staticNodes)
+    copyFile(path.normalize(config.network.genesisFile), genesisDestination)
+    copyFile(path.join(keyFolder, 'key'), path.join(keyDir, 'key'))
+    copyFile(path.join(keyFolder, 'nodekey'), path.join(gethDir, 'nodekey'))
+    copyFile(path.join(keyFolder, 'password.txt'), passwordDestination)
+    copyFile(path.join(keyFolder, 'tm.key'), path.join(tmDir, 'tm.key'))
+    copyFile(path.join(keyFolder, 'tm.pub'), path.join(tmDir, 'tm.pub'))
 
-    const { verbosity, id, consensus } = config.network
-    const { devP2pPort, rpcPort, wsPort, raftPort } = node.quorum
+    const initCommand = `cd ${networkPath} && geth --datadir ${quorumDir} init ${genesisDestination}`
+    initCommands.push(initCommand)
 
-    const args = `--nodiscover --rpc --rpccorsdomain=* --rpcvhosts=* --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,${consensus},quorumPermission --emitcheckpoints --unlock 0 --password qdata/dd${nodeNumber}/keystore/password.txt`
-    const consensusArgs = consensus === 'raft' ?
-      `--raft --raftport ${raftPort}` :
-      `--istanbul.blockperiod 5 --syncmode full --mine --minerthreads 1`
-
-    const startCommand = `PRIVATE_CONFIG=ignore nohup geth --datadir qdata/dd${nodeNumber} ${args} ${consensusArgs} --permissioned --verbosity ${verbosity} --networkid ${id} --rpcport ${rpcPort} --port ${devP2pPort} 2>>qdata/logs/${nodeNumber}.log &`
+    const startCommand = createGethStartCommand(config, node,
+      passwordDestination,
+      nodeNumber)
     startCommands.push(startCommand)
   })
 
-  config.nodes.forEach((node, i) => {
+  writeFile(path.join(networkPath, 'start.sh'), startCommands.join('\n'), true)
+  copyFile(path.join(process.cwd(), 'lib/stop.sh'),
+    path.join(networkPath, 'stop.sh'))
+
+  // initialize all the nodes
+  initCommands.forEach((command) => {
     // TODO figure out the safest way to run shell commands
-    let nodeNumber = i + 1
-    exec(
-      `cd ${path} && geth --datadir qdata/dd${nodeNumber} init qdata/dd${nodeNumber}/genesis.json`,
-      (e, stdout, stderr) => {
-        if (e instanceof Error) {
-          console.error(e)
-          throw e
-        }
-      })
+    exec(command, (e, stdout, stderr) => {
+      if (e instanceof Error) {
+        console.error(e)
+        throw e
+      }
+    })
   })
-  fs.writeFileSync(`${path}/start.sh`, startCommands.join('\n'))
-  fs.chmodSync(`${path}/start.sh`, '755')
-  fs.copyFileSync(`lib/stop.sh`, `${path}/stop.sh`)
 }
 
-function generateStaticNodes (nodes, consensus) {
+function createStaticNodes (nodes, consensus) {
   return nodes.map((node, i) => {
     const nodeNumber = i + 1
     const generatedKeyFolder = `7nodes/key${nodeNumber}`
-    const enodeId = fs.readFileSync(`${generatedKeyFolder}/enode`,
+    const enodeId = fs.readFileSync(path.join(generatedKeyFolder, 'enode'),
       'utf8').trim()
 
     let enodeAddress = `enode://${enodeId}@127.0.0.1:${node.quorum.devP2pPort}?discport=0`
@@ -84,3 +92,17 @@ function generateStaticNodes (nodes, consensus) {
     return enodeAddress
   })
 }
+
+function createGethStartCommand (config, node, passwordDestination,
+  nodeNumber) {
+  const { verbosity, id, consensus } = config.network
+  const { devP2pPort, rpcPort, wsPort, raftPort } = node.quorum
+
+  const args = `--nodiscover --rpc --rpccorsdomain=* --rpcvhosts=* --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,${consensus},quorumPermission --emitcheckpoints --unlock 0 --password ${passwordDestination}`
+  const consensusArgs = consensus === 'raft' ?
+    `--raft --raftport ${raftPort}` :
+    `--istanbul.blockperiod 5 --syncmode full --mine --minerthreads 1`
+
+  return `PRIVATE_CONFIG=ignore nohup geth --datadir qdata/dd${nodeNumber} ${args} ${consensusArgs} --permissioned --verbosity ${verbosity} --networkid ${id} --rpcport ${rpcPort} --port ${devP2pPort} 2>>qdata/logs/${nodeNumber}.log &`
+}
+
