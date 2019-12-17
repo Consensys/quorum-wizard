e261129c003bda12724b465ec8ea9eb79a263827
apratt3377 <apratt3377@gmail.com>
2019-12-17 14:49:44 -0500

    cleanup and add execsync to executils

--- src/utils/execUtils.js
+++ src/utils/execUtils.js
@@ -1,8 +1,17 @@
-import { exec } from 'child_process'
+import { exec, execSync } from 'child_process'
 
 export function execute (command, callback) {
   exec(command, (e, stdout, stderr) => {
     if (e instanceof Error) {
+      console.error(e)
+      throw e
+    }
+  })
+}
+
+export function executeSync(command, callback) {
+  execSync(command, (e, stdout, stderr) => {
+    if(e instanceof Error) {
       console.error(e)
       throw e
     }
