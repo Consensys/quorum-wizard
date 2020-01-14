c2c32e16c81b11d389bc0b90990a759cc70b04fd
apratt3377 <apratt3377@gmail.com>
2020-01-14 10:56:46 -0500

    refactor docker and add test

--- src/utils/dockerHelper.js
+++ src/utils/dockerHelper.js
@@ -11,110 +11,23 @@ import {
 import { isTessera, createStaticNodes } from './networkCreator'
 import { generateKeys } from './keyGen'
 import { generateConsensusConfig } from '../model/ConsensusConfig'
+const yaml = require('js-yaml')
 
 export function buildDockerComposeWithTessera (config) {
   const definitions = readFileToString(join(process.cwd(), 'lib/docker-compose-definitions-tessera.yml'))
+  let services = config.nodes.map((node, i) =>
+    [buildNodeService(node, i, true), buildTesseraService(node, i)].join(""))
 
-  let services = config.nodes.map((node, i) =>`
-  node${i + 1}:
-    << : *quorum-def
-    hostname: node${i + 1}
-    ports:
-      - "${node.quorum.rpcPort}:8545"
-    volumes:
-      - vol${i + 1}:/qdata
-      - ./qdata:/examples:ro
-    depends_on:
-      - txmanager${i + 1}
-    environment:
-      - PRIVATE_CONFIG=/qdata/tm/tm.ipc
-      - NODE_ID=${i + 1}
-    networks:
-      quorum-examples-net:
-        ipv4_address: 172.16.239.1${i + 1}
-  txmanager${i + 1}:
-    << : *tx-manager-def
-    hostname: txmanager${i + 1}
-    ports:
-      - "${node.tm.thirdPartyPort}:9080"
-    volumes:
-      - vol${i + 1}:/qdata
-      - ./qdata:/examples:ro
-    networks:
-      quorum-examples-net:
-        ipv4_address: 172.16.239.10${i + 1}
-    environment:
-      - NODE_ID=${i + 1}`).join("")
-  const end = `
-#  cakeshop:
-#    << : *cakeshop-def
-#    hostname: cakeshop
-#    ports:
-#      - "8999:8999"
-#    volumes:
-#      - cakeshopvol:/qdata
-#      - ./qdata:/examples:ro
-#    networks:
-#      quorum-examples-net:
-#        ipv4_address: 172.16.239.186
-networks:
-  quorum-examples-net:
-    name: quorum-examples-net
-    driver: bridge
-    ipam:
-      driver: default
-      config:
-        - subnet: 172.16.239.0/24
-volumes:
-${config.nodes.map((_, i) => `  "vol${i + 1}":`).join("\n")}
-  "cakeshopvol":`
-
-  return [definitions, services, end].join("")
+  return [definitions, services.join(""), buildEndService(config)].join("")
 }
 
 export function buildDockerComposeNoTessera (config) {
   const definitions = readFileToString(join(process.cwd(), 'lib/docker-compose-definitions.yml'))
 
-  let services = config.nodes.map((node, i) =>`
-  node${i + 1}:
-    << : *quorum-def
-    hostname: node${i + 1}
-    ports:
-      - "${node.quorum.rpcPort}:8545"
-    volumes:
-      - vol${i + 1}:/qdata
-      - ./qdata:/examples:ro
-    environment:
-      - PRIVATE_CONFIG=ignore
-      - NODE_ID=${i + 1}
-    networks:
-      quorum-examples-net:
-        ipv4_address: 172.16.239.1${i + 1}`).join("")
-  const end = `
-#  cakeshop:
-#    << : *cakeshop-def
-#    hostname: cakeshop
-#    ports:
-#      - "8999:8999"
-#    volumes:
-#      - cakeshopvol:/qdata
-#      - ./qdata:/examples:ro
-#    networks:
-#      quorum-examples-net:
-#        ipv4_address: 172.16.239.186
-networks:
-  quorum-examples-net:
-    name: quorum-examples-net
-    driver: bridge
-    ipam:
-      driver: default
-      config:
-        - subnet: 172.16.239.0/24
-volumes:
-${config.nodes.map((_, i) => `  "vol${i + 1}":`).join("\n")}
-  "cakeshopvol":`
+  let services = config.nodes.map((node, i) =>
+    buildNodeService(node, i, false))
 
-  return [definitions, services, end].join("")
+  return [definitions, services.join(""), buildEndService(config)].join("")
 }
 
 export function createDockerCompose(config) {
@@ -179,4 +92,72 @@ export function createDockerCompose(config) {
   copyFile(join(process.cwd(), 'lib/public-contract.js'), join(qdata, 'public-contract.js'))
   copyFile(join(process.cwd(), 'lib/private-contract.js'), join(qdata, 'private-contract.js'))
 
+}
+
+function buildNodeService(node, i, hasTessera) {
+  const txManager = hasTessera ?
+    `depends_on:
+      - txmanager${i + 1}
+    environment:
+      - PRIVATE_CONFIG=/qdata/tm/tm.ipc` :
+    `environment:
+      - PRIVATE_CONFIG=ignore`
+
+  return `
+  node${i + 1}:
+    << : *quorum-def
+    hostname: node${i + 1}
+    ports:
+      - "${node.quorum.rpcPort}:8545"
+    volumes:
+      - vol${i + 1}:/qdata
+      - ./qdata:/examples:ro
+    ${txManager}
+      - NODE_ID=${i + 1}
+    networks:
+      quorum-examples-net:
+        ipv4_address: 172.16.239.1${i + 1}`
+}
+
+function buildTesseraService(node, i) {
+  return `
+  txmanager${i + 1}:
+    << : *tx-manager-def
+    hostname: txmanager${i + 1}
+    ports:
+      - "${node.tm.thirdPartyPort}:9080"
+    volumes:
+      - vol${i + 1}:/qdata
+      - ./qdata:/examples:ro
+    networks:
+      quorum-examples-net:
+        ipv4_address: 172.16.239.10${i + 1}
+    environment:
+      - NODE_ID=${i + 1}`
+}
+
+function buildEndService(config) {
+  return `
+#  cakeshop:
+#    << : *cakeshop-def
+#    hostname: cakeshop
+#    ports:
+#      - "8999:8999"
+#    volumes:
+#      - cakeshopvol:/qdata
+#      - ./qdata:/examples:ro
+#    networks:
+#      quorum-examples-net:
+#        ipv4_address: 172.16.239.186
+networks:
+  quorum-examples-net:
+    name: quorum-examples-net
+    driver: bridge
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.16.239.0/24
+volumes:
+${config.nodes.map((_, i) => `  "vol${i + 1}":`).join("\n")}
+  "cakeshopvol":`
 }
