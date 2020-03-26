217288bc1bad4541a5a8cba2eb39550ee29b7bc0
bmcd87 <bradmcdermott@gmail.com>
2020-03-26 11:40:57 -0400

    Destructures fs object in fileUtils

--- src/utils/fileUtils.js
+++ src/utils/fileUtils.js
@@ -1,4 +1,12 @@
-import fs from 'fs-extra'
+import {
+  existsSync,
+  writeFileSync,
+  chmodSync,
+  removeSync,
+  mkdirSync,
+  copyFileSync,
+  readFileSync,
+} from 'fs-extra'
 import {
   join,
   resolve,
@@ -17,17 +25,17 @@ export function libRootDir() {
 }
 
 export function exists(path) {
-  return fs.existsSync(path)
+  return existsSync(path)
 }
 
 export function writeJsonFile(folder, filename, object, space = 2) {
-  fs.writeFileSync(join(folder, filename), JSON.stringify(object, null, space))
+  writeFileSync(join(folder, filename), JSON.stringify(object, null, space))
 }
 
 export function writeFile(filePath, contents, executable = false) {
-  fs.writeFileSync(filePath, contents)
+  writeFileSync(filePath, contents)
   if (executable) {
-    fs.chmodSync(filePath, '755')
+    chmodSync(filePath, '755')
   }
 }
 
@@ -39,20 +47,20 @@ export function removeFolder(networkPath = '') {
   }
 
   if (exists(networkPath)) {
-    fs.removeSync(networkPath)
+    removeSync(networkPath)
   }
 }
 
 export function createFolder(path, recursive = false) {
-  fs.mkdirSync(path, { recursive })
+  mkdirSync(path, { recursive })
 }
 
 export function copyFile(src, dest) {
-  fs.copyFileSync(src, dest)
+  copyFileSync(src, dest)
 }
 
 export function readFileToString(file) {
-  return fs.readFileSync(file, 'utf8').trim()
+  return readFileSync(file, 'utf8').trim()
 }
 
 export function formatNewLine(file) {
