9fa992054d4cb49599094ca979fffbbd6d1497d7
apratt3377 <apratt3377@gmail.com>
2020-05-06 14:01:53 -0400

    fix qdata path

--- src/generators/networkCreator.js
+++ src/generators/networkCreator.js
@@ -68,6 +68,7 @@ export function generateResourcesRemote(config) {
       dockerCommand += `
       sed -i '' 's,%THIS_PRIV_KEY%,/qdata/tm/tm.key,g' ${networkPath}/out/config/tessera-config-9.0.json
       sed -i '' 's,%THIS_PUB_KEY%,/qdata/tm/tm.pub,g' ${networkPath}/out/config/tessera-config-9.0.json
+      sed -i '' 's,etc/quorum/,,g' ${networkPath}/out/config/tessera-config-9.0.json
       sed -i '' 's/%QUORUM-NODE\\([0-9]\\)_SERVICE_HOST%/txmanager\\1/g' ${networkPath}/out/config/tessera-config-9.0.json`
     }
   }
