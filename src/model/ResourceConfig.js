c862a9c88e5fb6b0ece1ab3336cf2cf0f4594f3e
Brad McDermott <bradmcdermott@gmail.com>
2020-07-24 11:59:06 -0400

    Updates kubernetes config for version 0.1.3-rc1

--- src/model/ResourceConfig.js
+++ src/model/ResourceConfig.js
@@ -1,4 +1,4 @@
-export const LATEST_QUBERNETES = 'v0.1.2'
+export const LATEST_QUBERNETES = 'v0.1.3-rc1'
 
 // eslint-disable-next-line import/prefer-default-export
 export function buildKubernetesResource(config) {
@@ -7,34 +7,28 @@ export function buildKubernetesResource(config) {
     buildQuorumDetails(config),
     buildTesseraDetails(config),
     buildGethDetails(config),
+    buildKubernetesDetails(config),
   ].join('')
 }
 
 function buildGeneralDetails(config) {
   const nodeNum = config.nodes.length
-  return `#namespace:
-#  name: quorum-test
-sep_deployment_files: true
+  return `sep_deployment_files: true
 nodes:
-  number: ${nodeNum}
-service:
-  type: NodePort`
+  number: ${nodeNum}`
 }
 
 function buildQuorumDetails(config) {
   return `
 quorum:
-  consensus: ${config.network.consensus}
   Node_DataDir: /etc/quorum/qdata
   Key_Dir_Base: ${config.network.generateKeys ? 'out/config' : '7nodes'}
   Permissioned_Nodes_File: out/config/permissioned-nodes.json
   Genesis_File: out/config/genesis.json
   quorum:
+    consensus: ${config.network.consensus}
     Raft_Port: ${config.containerPorts.quorum.raftPort}
-    Quorum_Version: ${config.network.quorumVersion}
-  storage:
-    Type: PVC
-    Capacity: 200Mi`
+    Quorum_Version: ${config.network.quorumVersion}`
 }
 
 function buildGethDetails(config) {
@@ -57,4 +51,14 @@ function buildTesseraDetails(config) {
     Port: ${config.containerPorts.tm.p2pPort}
     3Party_Port: ${config.containerPorts.tm.thirdPartyPort}
     Tessera_Config_Dir: out/config`
+}
+
+function buildKubernetesDetails(config) {
+  return `
+k8s:
+  service:
+    type: NodePort
+  storage:
+    Type: PVC
+    Capacity: 200Mi`
 }
