759990204d5be93fe5617cfef539d23dad0e3bff
Jayson <j4ys0n@gmail.com>
2020-07-30 21:53:05 -0600

    complete startup options

--- src/generators/scripts/start.js
+++ src/generators/scripts/start.js
@@ -22,8 +22,12 @@ export default {
 }
 
 function startScriptDocker(config) {
-  if (config.network.txGenerate) {
+  if (config.network.splunk && config.network.txGenerate) {
     return readFileToString(joinPath(libRootDir(), 'lib', 'start-with-splunk-txns.sh'))
+  } else if (config.network.splunk && !config.network.txGenerate) {
+    return readFileToString(joinPath(libRootDir(), 'lib', 'start-with-splunk.sh'))
+  } else if (!config.network.splunk && config.network.txGenerate) {
+    return readFileToString(joinPath(libRootDir(), 'lib', 'start-with-txns.sh'))
   }
   return `${scriptHeader()}
 docker-compose up -d`
