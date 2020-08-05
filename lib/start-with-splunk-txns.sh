4809f8243378a78ac7bf833685c9ebc6c2faf8ff
Jayson <j4ys0n@gmail.com>
2020-08-05 14:07:47 -0600

    clean up splunk dashboards

--- lib/start-with-splunk-txns.sh
+++ lib/start-with-splunk-txns.sh
@@ -9,9 +9,6 @@ docker-compose -f docker-compose-splunk.yml up -d
 
 sleep 3
 
-docker exec splunk /bin/bash -c 'sudo mkdir -p /opt/splunk/etc/apps/search/local/data/ui/views/'
-docker exec splunk /bin/bash -c 'sudo cp /dashboards/*.xml /opt/splunk/etc/apps/search/local/data/ui/views/'
-
 echo -n 'Waiting for splunk to start.'
 until docker logs splunk | grep -m 1 'Ansible playbook complete'
 do
